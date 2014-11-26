34027370b5d542cc8d51436a2765563c36ead835
kirmaniarslan <arslankirmani9600@gmail.com>
2014-11-26 14:06:17 +0100

    BoardConfig.mk: Disable Ril Class temporarily

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,7 @@ TARGET_RECOVERY_FSTAB := device/htc/pico/recovery.fstab
 
 # RIL
 BOARD_PROVIDES_LIBRIL := true
-BOARD_RIL_CLASS := ../../../device/htc/pico/ril
+#BOARD_RIL_CLASS := ../../../device/htc/pico/ril
 BOARD_USES_LEGACY_RIL := true
 BOARD_RIL_FIVE_SEARCH_RESPONSES :=true
 
